2a55e5b2e1515e605fcc7f6dcb5ab1d839982838
gorzer <gorzer.rostyslav@gmail.com>
2021-03-06 13:36:36 +0200

    Updates

---
+++ precache-manifest.fab144892519f72ca823be337c87256e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d328954ab0b1c9b8aa76cef485d1fc2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "28b46df821905bedc6c8",
+    "url": "./static/css/2.36dbc3cc.chunk.css"
+  },
+  {
+    "revision": "d421a7a3c2762a6e96c5",
+    "url": "./static/css/main.ba2fba33.chunk.css"
+  },
+  {
+    "revision": "28b46df821905bedc6c8",
+    "url": "./static/js/2.2b1a7771.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.2b1a7771.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d421a7a3c2762a6e96c5",
+    "url": "./static/js/main.c6f1bab7.chunk.js"
+  },
+  {
+    "revision": "3cfb45b61c015a055433",
+    "url": "./static/js/runtime-main.0527da25.js"
+  }
+]);
